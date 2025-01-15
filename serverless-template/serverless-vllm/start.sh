c3fee165aa9397bbe5f1773d00057ec1b07233a8
Kun Zhao <zhaokun.km@gmail.com>
2025-01-15 14:01:53 +0000

    Update Dockerfile, using torch-2.5.1 and bitsandbytes must great then 0.45

--- serverless-template/serverless-vllm/start.sh
+++ serverless-template/serverless-vllm/start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 echo "Container Started"
-export PYTHONUNBUFFERED=1
 
-source /workspace/vllm/venv/bin/activate
+export PYTHONUNBUFFERED=1
 
 vllm serve /workspace/vllm/phi-4-bnb-4bit --dtype bfloat16 --load_format bitsandbytes --quantization bitsandbytes --port 11434 &
 
-/workspace/vllm/venv/bin/python -u /workspace/vllm/src/main.py &
+exec python -u /workspace/vllm/src/main.py 
 
-exec sleep infinity
